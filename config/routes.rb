c29a8d0eddf8e7878b90aa9efca83dfe34a07524
Daniel Castro <danielsobretudo@yahoo.com.br>
2019-11-21 08:31:54 -0300

    pretty advance

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # get 'reviews/new'
+  # get 'reviews/create'
+  # get 'restaurants/index'
+  # get 'restaurants/new'
+  # get 'restaurants/create'
+  # get 'restaurants/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'restaurants', to: 'restaurants#index', as: :restaurants
+  get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_restaurant_review
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
+
+  # ou entao:
+  # resources :restaurants, only %i[index new create show] do
+  #   resources :reviews, only %i[ne create]
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Assador', address: 'Aterro', phone_number: '2222-2222', category: 'belgian')
+Restaurant.create(name: 'Amarelinho', address: 'Gloria', phone_number: '2222-2222', category: 'french')
+Restaurant.create(name: 'Xing Ling', address: 'China Town', phone_number: '2222-2222', category: 'chinese')
+Restaurant.create(name: 'Luigi', address: 'Laranjeiras', phone_number: '2222-2222', category: 'italian')
+Restaurant.create(name: 'Japa', address: 'Botafogo', phone_number: '2222-2222', category: 'japanese')
+Restaurant.create(name: 'Ruski', address: 'Asia', phone_number: '2222-2222', category: 'russian')
